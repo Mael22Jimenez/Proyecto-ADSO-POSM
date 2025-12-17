284eae7bfd54ec22d2c1d9a817dddb9909ea7c21
dbeltranq <dbeltranq@unal.edu.co>
2025-12-17 17:58:27 -0500

    Renderlizar host

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  host: '0.0.0.0',
-  port: Number(process.env.PORT) || 4173,
-  allowedHosts: ['proyecto-adso-posm.onrender.com'],
+  server: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 5173,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 4173,
+    allowedHosts: ['proyecto-adso-posm.onrender.com']
+  }
 })
-
