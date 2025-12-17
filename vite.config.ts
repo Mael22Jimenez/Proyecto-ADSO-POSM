3f5d6d07b5ca08cb3222c601c5c76dacc6e65b2b
dbeltranq <dbeltranq@unal.edu.co>
2025-12-17 17:53:49 -0500

    Change on config

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  host: '0.0.0.0',
+  port: Number(process.env.PORT) || 4173,
+  allowedHosts: ['proyecto-adso-posm.onrender.com'],
 })
 
-export default defineConfig({
-  preview: {
-    host: '0.0.0.0',
-    port: Number(process.env.PORT) || 4173,
-    allowedHosts: ['proyecto-adso-posm.onrender.com']
-  }
-});
