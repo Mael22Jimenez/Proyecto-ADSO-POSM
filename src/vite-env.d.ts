da40cf8c6eeda63cc6ebd87c120cfc6286e3dafd
dbeltranq <dbeltranq@unal.edu.co>
2025-12-18 15:52:39 -0500

    Added ENV for render service in FrontEnd

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,6 @@
-/// <reference types="vite/client" />
+const API_URL = import.meta.env.VITE_API_URL || "https://backend-posm.onrender.com";
+
+// Ahora en tus peticiones:
+fetch(`${API_URL}/api/productos`)
+fetch(`${API_URL}/login`)
+fetch(`${API_URL}/api/categorias`)
