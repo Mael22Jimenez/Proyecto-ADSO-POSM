18f7f5710714f2be2f6b58cd473f97a98c0119e6
dbeltranq <dbeltranq@unal.edu.co>
2025-12-18 15:59:12 -0500

    Check env path

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,1 @@
-const API_URL = import.meta.env.VITE_API_URL || "https://backend-posm.onrender.com";
-
-// Ahora en tus peticiones:
-fetch(`${API_URL}/api/productos`)
-fetch(`${API_URL}/login`)
-fetch(`${API_URL}/api/categorias`)
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3306";
